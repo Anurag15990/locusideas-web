670081c663c788eab054f9d6efb28f22d1552046
Anurag Agnihotri <anurag.agnihotri.159@gmail.com>
2016-08-15 17:03:19 +0530

    Add Auth Mechanism for login in the User

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,16 +1,15 @@
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import config from '../config/environment';
+
 
 /**
  * @method : Rest API Adapter for the Entire Application.
  */
-export default DS.JSONAPIAdapter.extend({
-
-  headers : {
-    "Accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1NzRiM2I5NzY4YTA3YjM0NWFlYzU1ZDMiLCJ0eXBlIjoiYXV0aGVudGljYXRpb24iLCJleHBpcmVzSW5TZWNvbmRzIjoiOTk5OTkiLCJpc3N1ZXIiOiIvL2FwaS1zdGFnaW5nLWMzcG8ubG9jdXNpZGVhcy5jb20iLCJhdWRpZW5jZSI6Ii8vYXBpLXN0YWdpbmctYzNwby5sb2N1c2lkZWFzLmNvbSIsImlhdCI6MTQ2ODQxNjExMn0.mGeQJvPq9OPqc5CXl4MhGJ8noDRiLRfZdl6ROMrgGk8"
-  },
-
-  host: 'http://api-staging-c3po.locusideas.com',
-  namespace: 'api'
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+  
+  host: `${config.host}`,
+  namespace: 'api',
+  authorizer:'authorizer:jwt'
 
 });
---
+++ app/authenticators/jwt.js
@@ -0,0 +1,56 @@
+import Ember from 'ember';
+import Base from 'ember-simple-auth/authenticators/base';
+import config from '../config/environment';
+
+const { RSVP: { Promise }, $: { ajax }, run } = Ember;
+
+export default Base.extend({
+  tokenEndpoint: `${config.host}/api/users/login`,
+
+  restore(data) {
+    return new Promise((resolve, reject) => {
+      if (!Ember.isEmpty(data.token)) {
+        resolve(data);
+      } else {
+        reject();
+      }
+    });
+  },
+
+  authenticate(creds) {
+    const { emailAddress, password } = creds;
+    const data = JSON.stringify({
+      emailAddress,
+      password
+    });
+
+    const requestOptions = {
+      url: this.tokenEndpoint,
+      type: 'POST',
+      data,
+      contentType: 'application/json',
+      dataType: 'json'
+    };
+
+    return new Promise((resolve, reject) => {
+      ajax(requestOptions).then((response) => {
+        const { token } = response;
+        // Wrapping aync operation in Ember.run
+        run(() => {
+          resolve({
+            token: token
+          });
+        });
+      }, (error) => {
+        // Wrapping aync operation in Ember.run
+        run(() => {
+          reject(error);
+        });
+      });
+    });
+  },
+
+  invalidate(data) {
+    return Promise.resolve(data);
+  }
+});
