073adb2f74bf977563ac0fcbc9184d651f436294
Anurag Agnihotri <anurag.agnihotri.159@gmail.com>
2016-08-18 11:53:12 +0530

    Route for designers

--- app/routes/designers.js
+++ app/routes/designers.js
@@ -1,40 +1,7 @@
 import Ember from 'ember';
-import config from '../config/environment';
-
-const { RSVP: { Promise }, $: { ajax }, run } = Ember;
 
 export default Ember.Route.extend({
-  session: Ember.inject.service(),
-
   model() {
-    const designersEndpoint = `${config.host}/api/users/designers`;
-    const headers = {};
-
-    this.get('session').authorize(('authorizer:jwt'), (headerName, headerValue) => {
-      headers[headerName] = headerValue;
-    });
-
-    const requestOptions = {
-      url: designersEndpoint,
-      type: "GET",
-      headers: headers,
-      contentType: 'application/json',
-      dataType: 'json'
-    };
-
-    return new Promise((resolve, reject) => {
-      ajax(requestOptions).then((response) => {
-        run(() => {
-          resolve({
-           response
-          });
-        });
-      }, (error) => {
-        run(() => {
-          reject(error);
-        });
-      });
-    });
-
+    return this.store.findAll('user');
   }
 });
